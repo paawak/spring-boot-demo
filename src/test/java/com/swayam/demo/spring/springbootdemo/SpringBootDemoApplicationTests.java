b22aad4047bab2dba713d0edaa1835ff7e6f6b18
Palash Ray <paawak@gmail.com>
2017-12-17 21:18:56 +0530

    re-instate

--- src/test/java/com/swayam/demo/spring/springbootdemo/SpringBootDemoApplicationTests.java
+++ src/test/java/com/swayam/demo/spring/springbootdemo/SpringBootDemoApplicationTests.java
@@ -1,12 +1,10 @@
 package com.swayam.demo.spring.springbootdemo;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@Ignore
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootDemoApplicationTests {
