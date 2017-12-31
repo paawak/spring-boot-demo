c8b4ec14c29989b3d02a1fc8799d58898eb79857
Palash Ray <paawak@gmail.com>
2017-12-31 21:54:56 +0530

    move to right package

--- restful-microservice/src/test/java/com/swayam/demo/springbootdemo/rest/RestFulMicroserviceApplicationTests.java
+++ restful-microservice/src/test/java/com/swayam/demo/springbootdemo/rest/RestFulMicroserviceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.swayam.demo.spring.springbootdemo;
+package com.swayam.demo.springbootdemo.rest;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootDemoApplicationTests {
+public class RestFulMicroserviceApplicationTests {
 
 	@Test
 	public void contextLoads() {
