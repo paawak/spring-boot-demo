5a3140d2b0b820f01f71705111dcc3e38c3bbb2f
Palash Ray <paawak@gmail.com>
2018-01-26 23:41:02 +0530

    initial setup for config server demo

---
+++ spring-config-server-demo/config-server-proxy/src/test/java/com/swayam/demo/springbootdemo/configserverproxy/ConfigServerProxyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.swayam.demo.springbootdemo.configserverproxy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConfigServerProxyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
