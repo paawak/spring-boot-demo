fcefc8e30ef6136508c56e10e56f9f75f2857dff
Palash Ray <paawak@gmail.com>
2018-03-03 23:43:56 +0530

    spring boot 2, skeleton

---
+++ reactive/reactive-demo/src/main/java/com/swayam/demo/springbootdemo/reactive/ReactiveDemoApplication.java
@@ -0,0 +1,12 @@
+package com.swayam.demo.springbootdemo.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveDemoApplication.class, args);
+	}
+}
