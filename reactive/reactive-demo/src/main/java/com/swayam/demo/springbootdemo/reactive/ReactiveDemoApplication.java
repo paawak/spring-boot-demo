498713b11f8e0d09ca483bd3b18080568f210cc8
Palash Ray <paawak@gmail.com>
2018-03-04 22:42:45 +0530

    remove redundant @EnableWebFlux

--- reactive/reactive-demo/src/main/java/com/swayam/demo/springbootdemo/reactive/ReactiveDemoApplication.java
+++ reactive/reactive-demo/src/main/java/com/swayam/demo/springbootdemo/reactive/ReactiveDemoApplication.java
@@ -2,9 +2,7 @@ package com.swayam.demo.springbootdemo.reactive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.reactive.config.EnableWebFlux;
 
-@EnableWebFlux
 @SpringBootApplication
 public class ReactiveDemoApplication {
 
