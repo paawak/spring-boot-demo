463a006d354916a6f576f32b35a700021f611d58
Palash Ray <paawak@gmail.com>
2018-03-23 08:43:57 +0530

    integrating grpc server with spring boot

---
+++ reactive/grpc-streaming-demo/src/main/java/com/swayam/demo/springbootdemo/grpc/config/GrpcServerConfig.java
@@ -0,0 +1,29 @@
+package com.swayam.demo.springbootdemo.grpc.config;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.swayam.demo.springbootdemo.grpc.controller.BankDetailStreamer;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+@Configuration
+public class GrpcServerConfig {
+
+	private static final Logger logger = LoggerFactory.getLogger(GrpcServerConfig.class);
+
+	@Bean(destroyMethod = "shutdown")
+	public Server server(@Value("${grpc.server.port}") int port, BankDetailStreamer bankDetailStreamer)
+			throws IOException {
+		Server server = ServerBuilder.forPort(port).addService(bankDetailStreamer).build().start();
+		logger.info("Grpc Server started, listening on " + port);
+		return server;
+	}
+
+}
