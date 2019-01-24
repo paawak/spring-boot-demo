1a0568f2c737c13c77a365613409e6f968b89c7b
Palash Ray <paawak@gmail.com>
2019-01-24 09:19:40 +0530

    split into profiles

---
+++ profile-switching-demo/src/main/resources/sql/dev/V2__bank_details_data.sql
@@ -0,0 +1,15 @@
+--
+-- Dumping data for table bank_details
+--
+
+INSERT INTO bank_details (id, age, job, marital, education, defaulted, balance, housing, loan, contact, day, month, duration, campaign, pdays, previous, poutcome, y) VALUES
+(1, 30, 'unemployed', 'married', 'primary', 'no', 1787, 'no', 'no', 'cellular', 19, 'oct', 79, 1, -1, 0, 'unknown', 'no'),
+(2, 33, 'services', 'married', 'secondary', 'no', 4789, 'yes', 'yes', 'cellular', 11, 'may', 220, 1, 339, 4, 'failure', 'no'),
+(3, 35, 'management', 'single', 'tertiary', 'no', 1350, 'yes', 'no', 'cellular', 16, 'apr', 185, 1, 330, 1, 'failure', 'no'),
+(4, 30, 'management', 'married', 'tertiary', 'no', 1476, 'yes', 'yes', 'unknown', 3, 'jun', 199, 4, -1, 0, 'unknown', 'no'),
+(5, 59, 'blue-collar', 'married', 'secondary', 'no', 0, 'yes', 'no', 'unknown', 5, 'may', 226, 1, -1, 0, 'unknown', 'no'),
+(6, 35, 'management', 'single', 'tertiary', 'no', 747, 'no', 'no', 'cellular', 23, 'feb', 141, 2, 176, 3, 'failure', 'no'),
+(7, 36, 'self-employed', 'married', 'tertiary', 'no', 307, 'yes', 'no', 'cellular', 14, 'may', 341, 1, 330, 2, 'other', 'no'),
+(8, 39, 'technician', 'married', 'secondary', 'no', 147, 'yes', 'no', 'cellular', 6, 'may', 151, 2, -1, 0, 'unknown', 'no'),
+(9, 41, 'entrepreneur', 'married', 'tertiary', 'no', 221, 'yes', 'no', 'unknown', 14, 'may', 57, 2, -1, 0, 'unknown', 'no'),
+(10, 43, 'services', 'married', 'primary', 'no', -88, 'yes', 'yes', 'cellular', 17, 'apr', 313, 1, 147, 2, 'failure', 'no');
---
+++ profile-switching-demo/src/main/resources/sql/prod/V2__bank_details_data.sql
@@ -0,0 +1,15 @@
+--
+-- Dumping data for table bank_details
+--
+
+INSERT INTO bank_details (id, age, job, marital, education, defaulted, balance, housing, loan, contact, day, month, duration, campaign, pdays, previous, poutcome, y) VALUES
+(11, 39, 'services', 'married', 'secondary', 'no', 9374, 'yes', 'no', 'unknown', 20, 'may', 273, 1, -1, 0, 'unknown', 'no'),
+(12, 43, 'admin.', 'married', 'secondary', 'no', 264, 'yes', 'no', 'cellular', 17, 'apr', 113, 2, -1, 0, 'unknown', 'no'),
+(13, 36, 'technician', 'married', 'tertiary', 'no', 1109, 'no', 'no', 'cellular', 13, 'aug', 328, 2, -1, 0, 'unknown', 'no'),
+(14, 20, 'student', 'single', 'secondary', 'no', 502, 'no', 'no', 'cellular', 30, 'apr', 261, 1, -1, 0, 'unknown', 'yes'),
+(15, 31, 'blue-collar', 'married', 'secondary', 'no', 360, 'yes', 'yes', 'cellular', 29, 'jan', 89, 1, 241, 1, 'failure', 'no'),
+(16, 40, 'management', 'married', 'tertiary', 'no', 194, 'no', 'yes', 'cellular', 29, 'aug', 189, 2, -1, 0, 'unknown', 'no'),
+(17, 56, 'technician', 'married', 'secondary', 'no', 4073, 'no', 'no', 'cellular', 27, 'aug', 239, 5, -1, 0, 'unknown', 'no'),
+(18, 37, 'admin.', 'single', 'tertiary', 'no', 2317, 'yes', 'no', 'cellular', 20, 'apr', 114, 1, 152, 2, 'failure', 'no'),
+(19, 25, 'blue-collar', 'single', 'primary', 'no', -221, 'yes', 'no', 'unknown', 23, 'may', 250, 1, -1, 0, 'unknown', 'no'),
+(20, 31, 'services', 'married', 'secondary', 'no', 132, 'no', 'no', 'cellular', 7, 'jul', 148, 1, 152, 1, 'other', 'no');
