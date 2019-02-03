3d9d26fcfba908fee64ffb0e4bb02221e791433c
Palash Ray <paawak@gmail.com>
2019-02-03 13:06:03 +0530

    renamed classes with main methods

--- reactive/grpc-streaming-demo/grpc-client/src/main/java/com/swayam/demo/springbootdemo/grpc/client/GrpcRestClientApplication.java
+++ reactive/grpc-streaming-demo/grpc-client/src/main/java/com/swayam/demo/springbootdemo/grpc/client/GrpcRestClientApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GrpcClientApplication {
+public class GrpcRestClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GrpcClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcRestClientApplication.class, args);
+    }
 }
