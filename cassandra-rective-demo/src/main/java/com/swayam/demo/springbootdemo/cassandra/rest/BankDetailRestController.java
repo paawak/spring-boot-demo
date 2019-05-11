08b14594edba8dfdc0b3c12ab1d9d8b3dc1ddcfe
Palash Ray <paawak@gmail.com>
2019-05-11 17:29:51 +0530

    group by

--- cassandra-rective-demo/src/main/java/com/swayam/demo/springbootdemo/cassandra/rest/BankDetailRestController.java
+++ cassandra-rective-demo/src/main/java/com/swayam/demo/springbootdemo/cassandra/rest/BankDetailRestController.java
@@ -10,8 +10,10 @@ import com.swayam.demo.springbootdemo.cassandra.model.BankDetail;
 import com.swayam.demo.springbootdemo.cassandra.repo.BankDetailRepository;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.GroupedFlux;
 
 @RestController
+@RequestMapping("/bank")
 public class BankDetailRestController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BankDetailRestController.class);
@@ -22,10 +24,16 @@ public class BankDetailRestController {
         this.bankDetailRepository = bankDetailRepository;
     }
 
-    @RequestMapping(value = "/bank", method = RequestMethod.GET)
-    public Flux<BankDetail> getBankDetailsReactive() {
+    @RequestMapping(method = RequestMethod.GET)
+    public Flux<BankDetail> getBankDetails() {
         LOGGER.info("serving reactive content");
         return bankDetailRepository.findAll();
+    }
+
+    @RequestMapping(value = "/group", method = RequestMethod.GET)
+    public Flux<String> getBankDetailsGrouped() {
+        LOGGER.info("grouping by");
+        return bankDetailRepository.findAll().groupBy(BankDetail::getJob).map(GroupedFlux::key);
     }
 
 }
