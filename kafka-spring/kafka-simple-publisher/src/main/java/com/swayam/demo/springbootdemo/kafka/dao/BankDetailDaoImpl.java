01b26b51a77c15968116bfdeec266b23700a75aa
Palash Ray <paawak@gmail.com>
2019-09-22 21:14:52 +0530

    remove delay in messages

--- kafka-spring/kafka-simple-publisher/src/main/java/com/swayam/demo/springbootdemo/kafka/dao/BankDetailDaoImpl.java
+++ kafka-spring/kafka-simple-publisher/src/main/java/com/swayam/demo/springbootdemo/kafka/dao/BankDetailDaoImpl.java
@@ -23,69 +23,59 @@ public class BankDetailDaoImpl implements BankDetailDao {
     private final JdbcTemplate jdbcTemplate;
 
     public BankDetailDaoImpl(JdbcTemplate jdbcTemplate) {
-	this.jdbcTemplate = jdbcTemplate;
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     @Override
     public List<BankDetail> getBankDetails() {
 
-	return jdbcTemplate.query(BANK_DETAILS_SQL, (ResultSet resultSet, int row) -> {
-	    return mapResultSet(resultSet);
-	});
+        return jdbcTemplate.query(BANK_DETAILS_SQL, (ResultSet resultSet, int row) -> {
+            return mapResultSet(resultSet);
+        });
 
     }
 
     @Override
     public void publishBankDetails(FluxSink<BankDetail> fluxSink) {
 
-	jdbcTemplate.query(BANK_DETAILS_SQL, (ResultSet rs) -> {
-	    LOGGER.info("start publishing...");
-	    int rowCount = 0;
-	    while (rs.next()) {
+        jdbcTemplate.query(BANK_DETAILS_SQL, (ResultSet rs) -> {
+            LOGGER.info("start publishing...");
+            while (rs.next()) {
 
-		if (fluxSink.isCancelled()) {
-		    LOGGER.info("publishing is cancelled");
-		    return null;
-		}
+                if (fluxSink.isCancelled()) {
+                    LOGGER.info("publishing is cancelled");
+                    return null;
+                }
 
-		fluxSink.next(mapResultSet(rs));
-		if (++rowCount % 2 == 0) {
-		    try {
-			LOGGER.info("in delay...");
-			Thread.sleep(1000);
-		    } catch (InterruptedException e) {
-			LOGGER.error("error", e);
-		    }
-		}
-
-	    }
-	    LOGGER.info("completed publishing");
-	    fluxSink.complete();
-	    return null;
-	});
+                fluxSink.next(mapResultSet(rs));
+            }
+            LOGGER.info("completed publishing");
+            fluxSink.complete();
+            return null;
+        });
     }
 
     private BankDetail mapResultSet(ResultSet resultSet) throws SQLException {
-	BankDetail bankDetail = new BankDetail();
-	bankDetail.setId(resultSet.getInt("id"));
-	bankDetail.setAge(resultSet.getInt("age"));
-	bankDetail.setJob(resultSet.getString("job"));
-	bankDetail.setMarital(resultSet.getString("marital"));
-	bankDetail.setEducation(resultSet.getString("education"));
-	bankDetail.setDefaulted(resultSet.getString("defaulted"));
-	bankDetail.setBalance(resultSet.getBigDecimal("balance"));
-	bankDetail.setHousing(resultSet.getString("housing"));
-	bankDetail.setLoan(resultSet.getString("loan"));
-	bankDetail.setContact(resultSet.getString("contact"));
-	bankDetail.setDay(resultSet.getInt("day"));
-	bankDetail.setMonth(resultSet.getString("month"));
-	bankDetail.setDuration(resultSet.getInt("duration"));
-	bankDetail.setCampaign(resultSet.getInt("campaign"));
-	bankDetail.setPdays(resultSet.getInt("pdays"));
-	bankDetail.setPrevious(resultSet.getInt("previous"));
-	bankDetail.setPoutcome(resultSet.getString("poutcome"));
-	bankDetail.setY(resultSet.getString("y"));
-	return bankDetail;
+        BankDetail bankDetail = new BankDetail();
+        bankDetail.setId(resultSet.getInt("id"));
+        bankDetail.setAge(resultSet.getInt("age"));
+        bankDetail.setJob(resultSet.getString("job"));
+        bankDetail.setMarital(resultSet.getString("marital"));
+        bankDetail.setEducation(resultSet.getString("education"));
+        bankDetail.setDefaulted(resultSet.getString("defaulted"));
+        bankDetail.setBalance(resultSet.getBigDecimal("balance"));
+        bankDetail.setHousing(resultSet.getString("housing"));
+        bankDetail.setLoan(resultSet.getString("loan"));
+        bankDetail.setContact(resultSet.getString("contact"));
+        bankDetail.setDay(resultSet.getInt("day"));
+        bankDetail.setMonth(resultSet.getString("month"));
+        bankDetail.setDuration(resultSet.getInt("duration"));
+        bankDetail.setCampaign(resultSet.getInt("campaign"));
+        bankDetail.setPdays(resultSet.getInt("pdays"));
+        bankDetail.setPrevious(resultSet.getInt("previous"));
+        bankDetail.setPoutcome(resultSet.getString("poutcome"));
+        bankDetail.setY(resultSet.getString("y"));
+        return bankDetail;
     }
 
 }
