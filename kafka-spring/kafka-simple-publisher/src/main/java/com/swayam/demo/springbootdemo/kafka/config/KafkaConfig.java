106deb52520b6d6a0b5a0a6a118011bc58a681d9
Palash Ray <paawak@gmail.com>
2019-10-10 01:16:40 +0530

    should not be needed anymore, do not publish type info un-necessarily

--- kafka-spring/kafka-simple-publisher/src/main/java/com/swayam/demo/springbootdemo/kafka/config/KafkaConfig.java
+++ kafka-spring/kafka-simple-publisher/src/main/java/com/swayam/demo/springbootdemo/kafka/config/KafkaConfig.java
@@ -5,12 +5,11 @@ import java.util.Map;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
-@Configuration
+//@Configuration
 public class KafkaConfig {
 
     @Bean
