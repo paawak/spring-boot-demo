9a194b78e5f748de64e297810cc02013bc5a194e
Palash Ray <paawak@gmail.com>
2019-11-03 21:06:22 +0530

    select records after time interval

--- kafka-spring/kafka-camel-demo/src/main/java/com/swayam/demo/springbootdemo/kafka/camel/service/BackfillScheduler.java
+++ kafka-spring/kafka-camel-demo/src/main/java/com/swayam/demo/springbootdemo/kafka/camel/service/BackfillScheduler.java
@@ -1,6 +1,8 @@
 package com.swayam.demo.springbootdemo.kafka.camel.service;
 
 import java.sql.ResultSet;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -8,6 +10,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class BackfillScheduler {
+
+    private static final int MIN_INTERVAL_MINUTES = 10;
 
     private final NamedParameterJdbcOperations jdbcTemplate;
     private final KafkaMessageReplayer kafkaMessageReplayer;
@@ -18,10 +22,13 @@ public class BackfillScheduler {
 	this.kafkaMessageReplayer = kafkaMessageReplayer;
     }
 
-    @Scheduled(initialDelay = 5000, fixedDelay = 10_000_000)
+    @Scheduled(initialDelay = 5000, fixedDelay = 1_000)
     public void checkForBackill() {
-	String sql = "SELECT * FROM message_outbox";
-	jdbcTemplate.query(sql, (ResultSet resultSet) -> {
+	String sql =
+		"SELECT * FROM message_outbox WHERE TIMESTAMPDIFF(MINUTE, insert_time, NOW()) > :intervalInMinutes";
+	Map<String, Object> params = new HashMap<>();
+	params.put("intervalInMinutes", MIN_INTERVAL_MINUTES);
+	jdbcTemplate.query(sql, params, (ResultSet resultSet) -> {
 	    String correlationId = resultSet.getString("correlation_id");
 	    String topicName = resultSet.getString("topic_name");
 	    int partitionId = resultSet.getInt("partition_id");
