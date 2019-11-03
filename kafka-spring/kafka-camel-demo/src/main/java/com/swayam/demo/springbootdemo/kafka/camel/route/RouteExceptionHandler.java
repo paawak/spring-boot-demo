9fb6bc95530b13f8432bac5a74ac0cc333a81fb7
Palash Ray <paawak@gmail.com>
2019-11-03 21:06:56 +0530

    desc

--- kafka-spring/kafka-camel-demo/src/main/java/com/swayam/demo/springbootdemo/kafka/camel/route/RouteExceptionHandler.java
+++ kafka-spring/kafka-camel-demo/src/main/java/com/swayam/demo/springbootdemo/kafka/camel/route/RouteExceptionHandler.java
@@ -1,14 +1,21 @@
 package com.swayam.demo.springbootdemo.kafka.camel.route;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RouteExceptionHandler extends RouteBuilder {
 
+    private static final Logger LOG = LoggerFactory.getLogger(RouteExceptionHandler.class);
+
     @Override
     public void configure() {
-	onException(Throwable.class).maximumRedeliveries(1).log("Exception occured").handled(true);
+	onException(Throwable.class).maximumRedeliveries(1).process(exchange -> {
+	    LOG.error("Exception occured in message with body: {} and headers: {}",
+		    exchange.getIn().getBody(), exchange.getIn().getHeaders());
+	}).handled(true);
     }
 
 }
