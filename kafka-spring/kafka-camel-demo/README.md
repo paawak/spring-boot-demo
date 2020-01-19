0b7d114c71d7ede5d925d377feaad0682718cf9d
Palash Ray <paawak@gmail.com>
2020-01-19 21:30:50 +0530

    added info

--- kafka-spring/kafka-camel-demo/README.md
+++ kafka-spring/kafka-camel-demo/README.md
@@ -2,7 +2,14 @@
 
 This is a demo to show how messages can be aggregated using Kafka/Camel/Spring
 
-## Running the app
+# Running the app
+
+## Start Kafka
+
+	bin/zookeeper-server-start.sh config/zookeeper.properties &
+	bin/kafka-server-start.sh config/server.properties &
+	
+## Start the application	
 
 The below environment variable should be set to uniquely identify this node.
 
@@ -11,3 +18,10 @@ The below environment variable should be set to uniquely identify this node.
 You can run another instance by changing the port
 
 	-Dserver.port=8080
+
+## Publish messages on Kafka
+
+Start the **KafkaSimplePublisherApplication** and the use the below command to publish messages on Kafka
+
+	curl "http://localhost:8090/kafka/publish"
+
--- kafka-spring/kafka-camel-demo/src/main/java/com/swayam/demo/springbootdemo/kafka/camel/service/BackfillScheduler.java
+++ kafka-spring/kafka-camel-demo/src/main/java/com/swayam/demo/springbootdemo/kafka/camel/service/BackfillScheduler.java
@@ -2,6 +2,7 @@ package com.swayam.demo.springbootdemo.kafka.camel.service;
 
 import java.sql.ResultSet;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -15,7 +16,7 @@ public class BackfillScheduler {
 
     private static final Logger LOG = LoggerFactory.getLogger(BackfillScheduler.class);
 
-    private static final int MIN_INTERVAL_MINUTES = 10;
+    private static final int MIN_INTERVAL_MINUTES = 3;
 
     private final NamedParameterJdbcOperations jdbcTemplate;
     private final KafkaMessageReplayer kafkaMessageReplayer;
@@ -33,15 +34,41 @@ public class BackfillScheduler {
 		"SELECT * FROM message_outbox WHERE TIMESTAMPDIFF(MINUTE, insert_time, NOW()) > :intervalInMinutes";
 	Map<String, Object> params = new HashMap<>();
 	params.put("intervalInMinutes", MIN_INTERVAL_MINUTES);
-	jdbcTemplate.query(sql, params, (ResultSet resultSet) -> {
-	    String correlationId = resultSet.getString("correlation_id");
-	    String topicName = resultSet.getString("topic_name");
-	    int partitionId = resultSet.getInt("partition_id");
-	    long offset = resultSet.getLong("offset");
-	    LOG.trace("topicName: {}, partitionId: {}, offset: {}, correlationId: {}", topicName,
-		    partitionId, offset, correlationId);
-	    kafkaMessageReplayer.replayMessages(topicName, partitionId, offset, correlationId);
-	});
+	List<MessageOutBox> outbox =
+		jdbcTemplate.query(sql, params, (ResultSet resultSet, int rowNum) -> {
+		    String correlationId = resultSet.getString("correlation_id");
+		    String topicName = resultSet.getString("topic_name");
+		    int partitionId = resultSet.getInt("partition_id");
+		    long offset = resultSet.getLong("offset");
+		    LOG.trace("topicName: {}, partitionId: {}, offset: {}, correlationId: {}",
+			    topicName, partitionId, offset, correlationId);
+		    return new MessageOutBox(correlationId, topicName, partitionId, offset);
+		});
+
+	if (outbox.isEmpty()) {
+	    LOG.info("No backfill found");
+	    return;
+	} else {
+	    LOG.info("{} records found for backfill", outbox.size());
+	}
+
+	outbox.forEach(messageOutBox -> kafkaMessageReplayer.replayMessages(messageOutBox.topicName,
+		messageOutBox.partitionId, messageOutBox.offset, messageOutBox.correlationId));
+
+    }
+
+    private static class MessageOutBox {
+	private final String correlationId;
+	private final String topicName;
+	private final int partitionId;
+	private final long offset;
+
+	MessageOutBox(String correlationId, String topicName, int partitionId, long offset) {
+	    this.correlationId = correlationId;
+	    this.topicName = topicName;
+	    this.partitionId = partitionId;
+	    this.offset = offset;
+	}
     }
 
 }
