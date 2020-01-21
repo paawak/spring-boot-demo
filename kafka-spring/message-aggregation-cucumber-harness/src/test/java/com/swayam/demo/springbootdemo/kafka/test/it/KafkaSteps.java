bf3b642aee25ea1b30b23f0f1f482d351de39fe0
Palash Ray <paawak@gmail.com>
2020-01-21 10:05:20 +0530

    working version

--- kafka-spring/message-aggregation-cucumber-harness/src/test/java/com/swayam/demo/springbootdemo/kafka/test/it/KafkaSteps.java
+++ kafka-spring/message-aggregation-cucumber-harness/src/test/java/com/swayam/demo/springbootdemo/kafka/test/it/KafkaSteps.java
@@ -1,24 +1,29 @@
-package com.swayam.demo.springbootdemo.kafka.test.glue;
+package com.swayam.demo.springbootdemo.kafka.test.it;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class UserSteps {
+public class KafkaSteps {
+
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaSteps.class);
 
     @Given("Bank details messages are published on Kafka {string}")
     public void bankDetailsMessagesPublishedOnKafka(String kafkaBrokers) {
-
+	LOG.info("11111");
     }
 
     @When("I wait for {int} mins")
     public void iWaitFor(int waitTimeInMinutes) {
-
+	LOG.info("22222");
     }
 
     @Then("Aggregation results are published")
     public void aggregationResultsPublished() {
-
+	LOG.info("33333");
     }
 
 }
--- kafka-spring/message-aggregation-cucumber-harness/src/test/resources/com/swayam/demo/springbootdemo/kafka/test/it/simple-words.feature
+++ kafka-spring/message-aggregation-cucumber-harness/src/test/resources/com/swayam/demo/springbootdemo/kafka/test/it/simple-words.feature
@@ -2,6 +2,6 @@
  	
 Scenario: Message aggregation works for happy path
   Given Bank details messages are published on Kafka "192.168.1.4:9092"
-  When I wait for "2" mins
+  When I wait for 2 mins
   Then Aggregation results are published
         
