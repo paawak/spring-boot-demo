599cf8a74ca26d8af3f4afc4e752f1496dafaba8
Palash Ray <paawak@gmail.com>
2020-01-21 10:44:48 +0530

    using lambdas

--- kafka-spring/message-aggregation-cucumber-harness/src/test/java/com/swayam/demo/springbootdemo/kafka/test/it/KafkaSteps.java
+++ kafka-spring/message-aggregation-cucumber-harness/src/test/java/com/swayam/demo/springbootdemo/kafka/test/it/KafkaSteps.java
@@ -3,27 +3,25 @@ package com.swayam.demo.springbootdemo.kafka.test.it;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import io.cucumber.java8.En;
 
-public class KafkaSteps {
+public class KafkaSteps implements En {
 
     private static final Logger LOG = LoggerFactory.getLogger(KafkaSteps.class);
 
-    @Given("Bank details messages are published on Kafka {string}")
-    public void bankDetailsMessagesPublishedOnKafka(String kafkaBrokers) {
-	LOG.info("11111");
-    }
+    public KafkaSteps() {
 
-    @When("I wait for {int} mins")
-    public void iWaitFor(int waitTimeInMinutes) {
-	LOG.info("22222");
-    }
+	Given("Bank details messages are published on Kafka {string}", (String kafkaBrokers) -> {
+	    LOG.info("kafkaBrokers: {}", kafkaBrokers);
+	});
 
-    @Then("Aggregation results are published")
-    public void aggregationResultsPublished() {
-	LOG.info("33333");
+	When("I wait for {int} mins", (Integer mins) -> {
+	    LOG.info("waiting for {} mins...", mins);
+	});
+
+	Then("Aggregation results are published", () -> {
+	    LOG.info("verifying results published...");
+	});
     }
 
 }
