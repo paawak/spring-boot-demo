4afdcb8e271d87ae0be015794033ba044797a54f
Palash Ray <paawak@gmail.com>
2020-01-21 11:15:03 +0530

    publishing kafka messages

--- kafka-spring/message-aggregation-cucumber-harness/src/test/java/com/swayam/demo/springbootdemo/kafka/test/it/AggregationSteps.java
+++ kafka-spring/message-aggregation-cucumber-harness/src/test/java/com/swayam/demo/springbootdemo/kafka/test/it/AggregationSteps.java
@@ -1,5 +1,10 @@
 package com.swayam.demo.springbootdemo.kafka.test.it;
 
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.apache.http.client.fluent.Request;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,6 +14,8 @@ public class AggregationSteps implements En {
 
     private static final Logger LOG = LoggerFactory.getLogger(AggregationSteps.class);
 
+    private final ExecutorService executorService = Executors.newFixedThreadPool(4);
+
     public AggregationSteps() {
 
 	Given("The kafka-camel-demo application is available at {string}", (String url) -> {
@@ -17,10 +24,29 @@ public class AggregationSteps implements En {
 
 	When("I publish bank details messages on Kafka by invoking {string}", (String url) -> {
 	    LOG.info("message publisher url: {}", url);
+	    Runnable task = () -> {
+		Request request = Request.Get(url);
+		int code;
+		try {
+		    code = request.execute().returnResponse().getStatusLine().getStatusCode();
+		} catch (IOException e) {
+		    throw new RuntimeException(e);
+		}
+		if (code == 200) {
+		    LOG.info("message publishing trigerred successfully");
+		} else {
+		    throw new IllegalArgumentException(
+			    "could not trigger message publishing as server returned a http code "
+				    + code);
+		}
+	    };
+
+	    executorService.submit(task);
 	});
 
 	When("I wait for {int} second", (Integer second) -> {
 	    LOG.info("waiting for {} second...", second);
+	    Thread.sleep(second * 1000);
 	});
 
 	Then("Aggregation results are published", () -> {
