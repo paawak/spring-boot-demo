cd65f9b65b07b74d24cc0ebf5ed8d90f4ea381d3
Palash Ray <paawak@users.noreply.github.com>
2021-09-26 23:19:19 +0530

    ByteBuddy with dynamic classes loaded in memory (#14)

---
+++ dynamic-classes-bytebuddy/in-memory-example/src/main/java/com/swayam/demo/springbootdemo/dynamicclassesbytebuddy/DynamicClassesBytebuddyInMemoryApplication.java
@@ -0,0 +1,13 @@
+package com.swayam.demo.springbootdemo.dynamicclassesbytebuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicClassesBytebuddyInMemoryApplication {
+
+    public static void main(String[] args) {
+	SpringApplication.run(DynamicClassesBytebuddyInMemoryApplication.class, args);
+    }
+
+}
---
+++ dynamic-classes-bytebuddy/in-memory-example/src/main/java/com/swayam/demo/springbootdemo/dynamicclassesbytebuddy/config/DynamicControllerPostProcessor.java
@@ -0,0 +1,62 @@
+package com.swayam.demo.springbootdemo.dynamicclassesbytebuddy.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.swayam.demo.springbootdemo.dynamicclassesbytebuddy.rest.BankDetailController;
+
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.description.annotation.AnnotationDescription;
+import net.bytebuddy.dynamic.DynamicType.Unloaded;
+import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
+
+@Configuration
+public class DynamicControllerPostProcessor implements BeanFactoryPostProcessor {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DynamicControllerPostProcessor.class);
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+	String className = BankDetailController.class.getName() + "V2";
+
+	Class<?> dynamicController = createDynamicController(className);
+
+	BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder
+		.rootBeanDefinition(dynamicController).addConstructorArgReference("bankDetailServiceImpl");
+	((DefaultListableBeanFactory) beanFactory).registerBeanDefinition(className,
+		beanDefinitionBuilder.getBeanDefinition());
+
+    }
+
+    private Class<?> createDynamicController(String className) {
+
+	LOG.info("Creating new class: {}", className);
+
+	Unloaded<?> generatedClass = new ByteBuddy().subclass(BankDetailController.class)
+		.annotateType(AnnotationDescription.Builder.ofType(RestController.class).build(),
+			AnnotationDescription.Builder.ofType(RequestMapping.class)
+				.defineArray("value", new String[] { "/v2/bank-item" }).build())
+		.name(className).make();
+
+	generatedClass.load(getClass().getClassLoader(), ClassLoadingStrategy.Default.INJECTION);
+
+	LOG.info("Loaded the class {} successfully into the classloader {}", className,
+		getClass().getClassLoader());
+
+	try {
+	    return Class.forName(className);
+	} catch (ClassNotFoundException e) {
+	    throw new RuntimeException(e);
+	}
+
+    }
+
+}
