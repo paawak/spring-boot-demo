e66a892744088f9f138cbca29097196579e20d19
Palash Ray <paawak@gmail.com>
2021-10-03 00:37:34 +0530

    replace flyway with liquibase

--- dynamic-classes-bytebuddy/dynamic-jpa-repo/src/main/java/com/swayam/demo/springbootdemo/dynamicclassesbytebuddy/DynamicJpaRepoApplication.java
+++ dynamic-classes-bytebuddy/dynamic-jpa-repo/src/main/java/com/swayam/demo/springbootdemo/dynamicclassesbytebuddy/DynamicJpaRepoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DynamicJpaRepoMemoryApplication {
+public class DynamicJpaRepoApplication {
 
     public static void main(String[] args) {
-	SpringApplication.run(DynamicJpaRepoMemoryApplication.class, args);
+	SpringApplication.run(DynamicJpaRepoApplication.class, args);
     }
 
 }
