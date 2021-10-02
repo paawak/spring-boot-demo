7cc7061e8629c64d0f45ba159a225c544ad4d985
Palash Ray <paawak@gmail.com>
2021-10-03 00:18:07 +0530

    cut 1

---
+++ dynamic-classes-bytebuddy/dynamic-jpa-repo/src/main/java/com/swayam/demo/springbootdemo/dynamicclassesbytebuddy/DynamicJpaRepoMemoryApplication.java
@@ -0,0 +1,13 @@
+package com.swayam.demo.springbootdemo.dynamicclassesbytebuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicJpaRepoMemoryApplication {
+
+    public static void main(String[] args) {
+	SpringApplication.run(DynamicJpaRepoMemoryApplication.class, args);
+    }
+
+}
