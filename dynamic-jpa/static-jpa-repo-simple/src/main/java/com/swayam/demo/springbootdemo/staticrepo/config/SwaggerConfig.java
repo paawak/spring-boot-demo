3520b644c88b24c7a6fc0ced214f633066d24d39
Palash Ray <paawak@gmail.com>
2021-10-03 13:47:13 +0530

    swagger

--- dynamic-jpa/static-jpa-repo-simple/src/main/java/com/swayam/demo/springbootdemo/staticrepo/config/SwaggerConfig.java
+++ dynamic-jpa/static-jpa-repo-simple/src/main/java/com/swayam/demo/springbootdemo/staticrepo/config/SwaggerConfig.java
@@ -27,7 +27,7 @@ public class SwaggerConfig {
     }
 
     private Docket docket(String version) {
-	ApiInfo apiInfo = new ApiInfoBuilder().title("Dynamic Classes Example " + version).version(version)
+	ApiInfo apiInfo = new ApiInfoBuilder().title("Static JPA Example " + version).version(version)
 		.contact(new Contact("Palash Ray", "https://palashray.com", "paawak@gmail.com")).build();
 	return new Docket(DocumentationType.SWAGGER_2).groupName(version).select().apis(RequestHandlerSelectors.any())
 		.paths(PathSelectors.ant("/" + version + "/*")).build().apiInfo(apiInfo);
