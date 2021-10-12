f5eb30e6884aaed1a5bf962cd902ded6839c348f
Palash Ray <paawak@gmail.com>
2021-10-12 15:09:43 +0530

    working with cucumber 6.x

---
+++ dynamic-jpa/static-jpa-repo-simple/src/test/java/com/swayam/demo/springbootdemo/staticrepo/RunCucumberTest.java
@@ -0,0 +1,12 @@
+package com.swayam.demo.springbootdemo.staticrepo;
+
+import io.cucumber.junit.platform.engine.Cucumber;
+
+@Cucumber
+// @Suite
+// @SelectClasspathResource("com/swayam")
+// @ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value =
+// "com.example")
+public class RunCucumberTest {
+
+}
