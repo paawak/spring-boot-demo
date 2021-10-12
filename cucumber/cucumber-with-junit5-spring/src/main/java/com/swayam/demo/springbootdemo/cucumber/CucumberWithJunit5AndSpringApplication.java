221433bbfb3496bceed61fde0d1d253400c85925
Palash Ray <paawak@gmail.com>
2021-10-12 22:35:28 +0530

    cucumber tests

---
+++ cucumber/cucumber-with-junit5-spring/src/main/java/com/swayam/demo/springbootdemo/cucumber/CucumberWithJunit5AndSpringApplication.java
@@ -0,0 +1,13 @@
+package com.swayam.demo.springbootdemo.cucumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumberWithJunit5AndSpringApplication {
+
+    public static void main(String[] args) {
+	SpringApplication.run(CucumberWithJunit5AndSpringApplication.class, args);
+    }
+
+}
---
+++ cucumber/cucumber-with-junit5-spring/src/test/java/com/swayam/demo/springbootdemo/cucumber/RunCucumberTest.java
@@ -0,0 +1,15 @@
+package com.swayam.demo.springbootdemo.cucumber;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import io.cucumber.core.options.Constants;
+
+@Suite
+@SelectClasspathResource("cukes")
+@ConfigurationParameter(key = Constants.PLUGIN_PUBLISH_QUIET_PROPERTY_NAME, value = "true")
+@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@book or @author")
+public class RunCucumberTest {
+
+}
