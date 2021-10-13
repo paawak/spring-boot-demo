78c1717e2bda208072f90b94a168b20a2b2cedb5
Palash Ray <paawak@gmail.com>
2021-10-14 00:48:46 +0530

    rename

--- dynamic-jpa/static-jpa-repo-abstraction/src/main/java/com/swayam/demo/springbootdemo/staticrepo/StaticJpaRepoAbstractionApplication.java
+++ dynamic-jpa/static-jpa-repo-abstraction/src/main/java/com/swayam/demo/springbootdemo/staticrepo/StaticJpaRepoAbstractionApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StaticJpaRepoSimpleApplication {
+public class StaticJpaRepoAbstractionApplication {
 
     public static void main(String[] args) {
-	SpringApplication.run(StaticJpaRepoSimpleApplication.class, args);
+	SpringApplication.run(StaticJpaRepoAbstractionApplication.class, args);
     }
 
 }
