3812b54f54f36d8da6aaf3c796b2eb16865c3c40
Palash Ray <paawak@gmail.com>
2021-10-14 10:36:36 +0530

    skeleton

--- dynamic-jpa/dynamic-jpa-repo/src/main/java/com/swayam/demo/springbootdemo/dynamicclassesbytebuddy/service/AuthorService.java
+++
@@ -1,9 +0,0 @@
-package com.swayam.demo.springbootdemo.dynamicclassesbytebuddy.service;
-
-import com.swayam.demo.springbootdemo.dynamicclassesbytebuddy.model.Author;
-
-public interface AuthorService {
-
-    Iterable<Author> getAuthors();
-
-}
