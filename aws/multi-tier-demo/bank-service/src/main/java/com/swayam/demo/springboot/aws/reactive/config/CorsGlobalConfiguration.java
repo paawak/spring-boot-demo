18552e5926eb177bed55e0fd621bf9fe0d56615e
Palash Ray <paawak@gmail.com>
2021-10-17 18:06:26 +0530

    externalise

--- aws/multi-tier-demo/bank-service/src/main/java/com/swayam/demo/springboot/aws/reactive/config/CorsGlobalConfiguration.java
+++ aws/multi-tier-demo/bank-service/src/main/java/com/swayam/demo/springboot/aws/reactive/config/CorsGlobalConfiguration.java
@@ -1,6 +1,7 @@
 package com.swayam.demo.springboot.aws.reactive.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
@@ -8,10 +9,16 @@ import org.springframework.web.reactive.config.WebFluxConfigurer;
 @Configuration
 public class CorsGlobalConfiguration implements WebFluxConfigurer {
 
+    private final Environment env;
+
+    public CorsGlobalConfiguration(Environment env) {
+	this.env = env;
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
-	corsRegistry.addMapping("/**").allowedOrigins("http://localhost:8100").allowedMethods(HttpMethod.GET.name())
-		.maxAge(3600);
+	corsRegistry.addMapping("/**").allowedOrigins(env.getProperty("bank-service.cors-allow-host"))
+		.allowedMethods(HttpMethod.GET.name()).maxAge(3600);
     }
 
 }
