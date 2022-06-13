191e5832fa9e39e2b1efe98825125d31d2093228
Palash Ray <paawak@users.noreply.github.com>
2022-06-13 08:24:32 +0530

    Liquibase Spring Boot Export Data (#34)
    Exporting data using Liquibase/SpringBoot CLI

---
+++ liquibase-demo/src/main/java/com/swayam/demo/springbootdemo/liquibase/LiquibaseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.swayam.demo.springbootdemo.liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseDemoApplication {
+
+    public static void main(String[] args) {
+	SpringApplication.run(LiquibaseDemoApplication.class, args);
+    }
+
+}
