cde9954db514667059188b5081e3f5b77bc655d7
Palash Ray <paawak@gmail.com>
2022-07-31 13:59:59 +0530

    spring cli

--- liquibase-demo/src/main/java/com/swayam/demo/springbootdemo/liquibase/LiquibaseDemoApplication.java
+++ liquibase-demo/src/main/java/com/swayam/demo/springbootdemo/liquibase/LiquibaseDemoApplication.java
@@ -1,13 +1,23 @@
 package com.swayam.demo.springbootdemo.liquibase;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiquibaseDemoApplication {
+public class LiquibaseDemoApplication implements CommandLineRunner {
+
+    @Autowired
+    private BatchedDBExporter batchedDBExporter;
 
     public static void main(String[] args) {
 	SpringApplication.run(LiquibaseDemoApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+	batchedDBExporter.export();
     }
 
 }
