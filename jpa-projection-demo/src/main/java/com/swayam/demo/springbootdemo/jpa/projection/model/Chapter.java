1d74dc28b2e2b229a56018e226b91563c7e9c79a
Palash Ray <paawak@gmail.com>
2024-04-25 00:30:30 +0530

    upgrade to jdk21/spring boot 3.2.5

--- jpa-projection-demo/src/main/java/com/swayam/demo/springbootdemo/jpa/projection/model/Chapter.java
+++ jpa-projection-demo/src/main/java/com/swayam/demo/springbootdemo/jpa/projection/model/Chapter.java
@@ -3,21 +3,20 @@ package com.swayam.demo.springbootdemo.jpa.projection.model;
 import java.io.Serializable;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
