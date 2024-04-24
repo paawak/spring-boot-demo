b2ed632ad58f4992b3aabe7168026e33743c0362
Palash Ray <paawak@users.noreply.github.com>
2024-04-25 01:28:53 +0530

    Feature/pr/jpa projection/1 (#61)
    * upgrade to jdk21/spring boot 3.2.5
    
    * added swagger
    
    * prevent serialization
    
    * added actuator, git
    
    * readme
    
    * print sql

--- jpa-projection-demo/src/main/java/com/swayam/demo/springbootdemo/jpa/projection/model/Author.java
+++ jpa-projection-demo/src/main/java/com/swayam/demo/springbootdemo/jpa/projection/model/Author.java
@@ -2,13 +2,12 @@ package com.swayam.demo.springbootdemo.jpa.projection.model;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
--- jpa-projection-demo/src/main/java/com/swayam/demo/springbootdemo/jpa/projection/model/Book.java
+++ jpa-projection-demo/src/main/java/com/swayam/demo/springbootdemo/jpa/projection/model/Book.java
@@ -3,22 +3,23 @@ package com.swayam.demo.springbootdemo.jpa.projection.model;
 import java.io.Serializable;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.Data;
 
+@JsonIgnoreProperties({ "hibernateLazyInitializer" })
 @Data
 @Entity
 @Table(name = "BOOK")
