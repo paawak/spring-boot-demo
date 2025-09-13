ee6102faa6fc4d7f5273d4e8df881dc5d0ca81e5
Palash Ray <paawak@gmail.com>
2025-09-14 00:35:01 +0530

    builder

--- dynamodb-demo/src/main/java/com/com/swayam/demo/springbootdemo/dynamodb/model/Section.java
+++ dynamodb-demo/src/main/java/com/com/swayam/demo/springbootdemo/dynamodb/model/Section.java
@@ -2,6 +2,12 @@ package com.com.swayam.demo.springbootdemo.dynamodb.model;
 
 import java.util.UUID;
 
-public record Section(UUID id, String sectionText, String style, int length) {
+import lombok.Builder;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbImmutable;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+
+@DynamoDbImmutable(builder = Section.SectionBuilder.class)
+@Builder
+public record Section(@DynamoDbPartitionKey UUID id, String sectionText, String style, int length) {
 
 }
