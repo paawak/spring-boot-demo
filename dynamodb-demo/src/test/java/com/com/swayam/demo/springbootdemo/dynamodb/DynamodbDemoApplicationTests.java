588bf973fc86f97676eb07ff5f2dd588942909f4
Palash Ray <paawak@gmail.com>
2025-09-13 21:26:12 +0530

    skaffold

---
+++ dynamodb-demo/src/test/java/com/com/swayam/demo/springbootdemo/dynamodb/DynamodbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.com.swayam.demo.springbootdemo.dynamodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamodbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
