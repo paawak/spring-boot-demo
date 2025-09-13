6603109269b1079b8f3c93edd11f1fc65345944a
Palash Ray <paawak@gmail.com>
2025-09-14 00:21:53 +0530

    annotations

--- dynamodb-demo/src/main/java/com/com/swayam/demo/springbootdemo/dynamodb/model/Section.java
+++ dynamodb-demo/src/main/java/com/com/swayam/demo/springbootdemo/dynamodb/model/Section.java
@@ -2,17 +2,6 @@ package com.com.swayam.demo.springbootdemo.dynamodb.model;
 
 import java.util.UUID;
 
-import lombok.Data;
-
-@Data
-public class Section {
-
-    private UUID id;
-
-    private String sectionText;
-
-    private String style;
-
-    private int length;
+public record Section(UUID id, String sectionText, String style, int length) {
 
 }
