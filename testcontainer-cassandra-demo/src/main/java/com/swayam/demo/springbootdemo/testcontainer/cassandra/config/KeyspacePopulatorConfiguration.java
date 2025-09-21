4514db841a9777ca9403735b9d2a4c4648fa37a4
Palash Ray <paawak@gmail.com>
2025-09-21 21:37:33 +0530

    disable config

--- testcontainer-cassandra-demo/src/main/java/com/swayam/demo/springbootdemo/testcontainer/cassandra/config/KeyspacePopulatorConfiguration.java
+++ testcontainer-cassandra-demo/src/main/java/com/swayam/demo/springbootdemo/testcontainer/cassandra/config/KeyspacePopulatorConfiguration.java
@@ -1,12 +1,11 @@
 package com.swayam.demo.springbootdemo.testcontainer.cassandra.config;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.core.cql.session.init.KeyspacePopulator;
 import org.springframework.data.cassandra.core.cql.session.init.ResourceKeyspacePopulator;
 
-@Configuration
+//@Configuration
 public class KeyspacePopulatorConfiguration extends AbstractCassandraConfiguration {
 
 	@Override
