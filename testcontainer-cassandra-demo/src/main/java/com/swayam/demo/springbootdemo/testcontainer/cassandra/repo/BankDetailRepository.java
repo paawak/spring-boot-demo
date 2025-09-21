c8c8804c34678848d152ac0b49cd2898fd9c865e
Palash Ray <paawak@gmail.com>
2025-09-21 20:48:21 +0530

    misc

--- testcontainer-cassandra-demo/src/main/java/com/swayam/demo/springbootdemo/testcontainer/cassandra/repo/BankDetailRepository.java
+++ testcontainer-cassandra-demo/src/main/java/com/swayam/demo/springbootdemo/testcontainer/cassandra/repo/BankDetailRepository.java
@@ -1,9 +1,11 @@
 package com.swayam.demo.springbootdemo.testcontainer.cassandra.repo;
 
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.swayam.demo.springbootdemo.testcontainer.cassandra.model.BankDetail;
 
+@Repository
 public interface BankDetailRepository extends ListCrudRepository<BankDetail, Integer> {
 
 }
