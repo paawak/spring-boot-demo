517faf1569a3305f6901fef783b540c3b853cb89
Palash Ray <paawak@gmail.com>
2025-09-21 21:08:26 +0530

    working somewhat

--- testcontainer-cassandra-demo/src/main/java/com/swayam/demo/springbootdemo/testcontainer/cassandra/TestcontainerCassandraDemoApplication.java
+++ testcontainer-cassandra-demo/src/main/java/com/swayam/demo/springbootdemo/testcontainer/cassandra/TestcontainerCassandraDemoApplication.java
@@ -2,14 +2,8 @@ package com.swayam.demo.springbootdemo.testcontainer.cassandra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
-
-import com.swayam.demo.springbootdemo.testcontainer.cassandra.model.BankDetail;
 
 @SpringBootApplication
-@EnableCassandraRepositories
-@EntityScan(basePackageClasses = BankDetail.class)
 public class TestcontainerCassandraDemoApplication {
 
 	public static void main(String[] args) {
