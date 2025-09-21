2975daa72f09b2a4ffe1ca855df9699e818da40b
Palash Ray <paawak@gmail.com>
2025-09-21 23:53:14 +0530

    renamed

--- testcontainer-cassandra-demo/src/main/java/com/swayam/demo/springbootdemo/testcontainer/cassandra/config/BankDetailDataLoaderConfiguration.java
+++ testcontainer-cassandra-demo/src/main/java/com/swayam/demo/springbootdemo/testcontainer/cassandra/config/BankDetailDataLoaderConfiguration.java
@@ -1,17 +1,23 @@
 package com.swayam.demo.springbootdemo.testcontainer.cassandra.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.core.cql.session.init.KeyspacePopulator;
 import org.springframework.data.cassandra.core.cql.session.init.ResourceKeyspacePopulator;
 
 //@Configuration
-public class KeyspacePopulatorConfiguration extends AbstractCassandraConfiguration {
+public class BankDetailDataLoaderConfiguration extends AbstractCassandraConfiguration {
+
+	private final String keySpaceName;
+
+	public BankDetailDataLoaderConfiguration(@Value("${spring.cassandra.keyspace-name}") String keySpaceName) {
+		this.keySpaceName = keySpaceName;
+	}
 
 	@Override
 	protected KeyspacePopulator keyspacePopulator() {
-		return new ResourceKeyspacePopulator(new ClassPathResource("/create_tables.cql"),
-				new ClassPathResource("bank_detail_data.cql"));
+		return new ResourceKeyspacePopulator(new ClassPathResource("/bank_detail_data.cql"));
 	}
 
 	@Override
@@ -21,7 +27,7 @@ public class KeyspacePopulatorConfiguration extends AbstractCassandraConfigurati
 
 	@Override
 	protected String getKeyspaceName() {
-		return "testcontainer_demo";
+		return keySpaceName;
 	}
 
 }
